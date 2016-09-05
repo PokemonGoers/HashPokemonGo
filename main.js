42db6b01bea2c168fee6c3c83fea558792d35594
Philipp Dowling <phdowling@users.noreply.github.com>
2016-09-05 23:55:07 +0200

    added websockets

--- main.js
+++ main.js
@@ -2,11 +2,14 @@
 /**
  * Created by dowling on 27/08/16.
  */
+
 require("../PokeData/constants");
 var Twitter = require('twitter');
 var hashpokemongo = require("./hashpokemongo");
 var twitterCredentials = require("./config/twitter.json");
 var PokemonTwitter = require("../PokeData/app/controllers/filler/twitter");
+
+
 
 // var params = {track: 'pokemon,pikachu,charmander,pokemongo,squirtle,bulbasaur'};  // TODO create better filter for pokemon go tweets (will probably get from project A anyway)
 var stream = PokemonTwitter.getPokemonTwitterStream();
@@ -14,9 +17,12 @@ var stream = PokemonTwitter.getPokemonTwitterStream();
 
 hashpokemongo.helloWorld();
 
-/*stream.on("data", function (data) {
-   console.log("got tweet by '" + data.user.screen_name +"': ", data.text, data.retweeted);
-});*/
+stream.on("data", function (data) {
+    if (data.user) {
+        var toLog = "got tweet by '" + data.user.screen_name + "': " + data.text;
+        console.log(toLog.replace("\n", ""), data.retweeted);
+    }
+});
 
 hashpokemongo.mob.startPokeMobDetection(stream, function (mob) {
     // TODO notify frontend of new mob
