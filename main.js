8d2f3bff0483243df55c6bc794418fd72db3d4b1
Philipp Dowling <phdowling@users.noreply.github.com>
2016-09-20 10:20:47 +0200

    activate mobdetection

--- main.js
+++ main.js
@@ -42,6 +42,7 @@ var mobOptions = {
 
 
 hashpokemongo.SentimentFeed({io: io}).startSentimentFeed(stream);
+hashpokemongo.MobDetection({io: io}).startPokeMobDetection(stream);
 
 
 //Lets require/import the HTTP module
